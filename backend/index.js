c60fe59ef318de95e812af539464e3cf8c783e6a
Hiomio <blessykaluri@gmail.com>
2025-08-29 21:57:45 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,15 @@ app.use(
       } else {
         callback(new Error("Not allowed by CORS"));
       }
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+
     },
   })
 );
